c82473a4c0003b7cf039ffa73de746ca3a484153
toniademosu <toni_ademosu@icloud.com>
2026-07-13 13:53:48 -0400

    Fix invisible galleries

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
       entries.forEach(function (e) {
         if (e.isIntersecting) { e.target.classList.add('in'); io.unobserve(e.target); }
       });
-    }, { threshold: 0.12 });
+    }, { threshold: 0, rootMargin: '0px 0px -60px 0px' });
     reveals.forEach(function (el) { io.observe(el); });
   } else {
     reveals.forEach(function (el) { el.classList.add('in'); });
